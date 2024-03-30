12fa20c0c84a5d1f584dc4781f175fcc5ac5a7f8
matt2ology <34324060+matt2ology@users.noreply.github.com>
2024-03-29 22:16:41 -0700

    Refactor day01.py: Delete old file and add new file with updated code

---
+++ day01_trebuchet/day01.py
@@ -0,0 +1,13 @@
+"""Day 1: The Tyranny of the Rocket Equation."""
+
+
+def main():
+    """Main entry point of the program.
+    """
+
+def sum_all_calibration_values(input_file: str) -> int:
+    
+
+
+if __name__ == '__main__':
+    main()
